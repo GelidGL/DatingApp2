849e9a6d7a353af828dc5ede278563dd7957abbf
GelidGL <egor.babinin.edu@gmail.com>
2022-07-30 21:58:07 +0300

    Account DTO JWT Authentication

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -2,12 +2,11 @@
 using DatingApp.Data;
 using DatingApp.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
 
 namespace DatingApp.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UsersController : ControllerBase
+    public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
 
@@ -17,11 +16,13 @@ namespace DatingApp.Controllers
         }
 
         [HttpGet]
+        [AllowAnonymous]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers() 
         {
             return await _context.Users.ToListAsync();
         }
 
+        [Authorize]
         [HttpGet("{id}")]
         public async Task<ActionResult<AppUser>> GetUser(int id) 
         {
